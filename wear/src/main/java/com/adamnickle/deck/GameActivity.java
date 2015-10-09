3f25875b4e6a213bdaa7e0e5850f0752db1c7608
Adam Nickle <anickle060193@gmail.com>
2015-10-08 22:03:33 -0500

    Minor changes to new wear module.

--- wear/src/main/java/com/adamnickle/deck/GameActivity.java
+++ wear/src/main/java/com/adamnickle/deck/GameActivity.java
@@ -3,25 +3,21 @@ package com.adamnickle.deck;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
 
 public class GameActivity extends Activity
 {
-
-    private TextView mTextView;
-
     @Override
     protected void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_game );
+
         final WatchViewStub stub = (WatchViewStub)findViewById( R.id.watch_view_stub );
         stub.setOnLayoutInflatedListener( new WatchViewStub.OnLayoutInflatedListener()
         {
             @Override
             public void onLayoutInflated( WatchViewStub stub )
             {
-                mTextView = (TextView)stub.findViewById( R.id.text );
             }
         } );
     }
