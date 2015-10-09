74961c90972abab38eee0455dfda8e655aa1495b
Adam Nickle <anickle060193@gmail.com>
2015-10-08 21:51:13 -0500

    Adding wear module.

---
+++ wear/src/main/java/com/adamnickle/deck/GameActivity.java
@@ -0,0 +1,28 @@
+package com.adamnickle.deck;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class GameActivity extends Activity
+{
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_game );
+        final WatchViewStub stub = (WatchViewStub)findViewById( R.id.watch_view_stub );
+        stub.setOnLayoutInflatedListener( new WatchViewStub.OnLayoutInflatedListener()
+        {
+            @Override
+            public void onLayoutInflated( WatchViewStub stub )
+            {
+                mTextView = (TextView)stub.findViewById( R.id.text );
+            }
+        } );
+    }
+}
